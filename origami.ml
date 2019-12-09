da993b5ab99e4d1739f3aac3444657850cc9c563
PePe <pszemek.prabucki@gmail.com>
2019-12-09 19:28:39 +0100

    look at my code, my code is amazing

--- origami.ml
+++ origami.ml
@@ -1,22 +1,26 @@
 (** Punkt na płaszczyźnie *)
 type point = float * float
 
-(** Poskładana kartka: ile razy kartkę przebije szpilka wbita w danym punkcie *)
+(** Poskładana kartka: ile razy kartkę przebije szpilka wbita
+	w danym punkcie *)
 type kartka = point -> int
 
-(** Epsilon oraz funkcja do sprawdzaniu rownosci float'ow*)
-let eps = 1e-8;;
+(** Epsilon oraz funkcja do sprawdzania rownosci float'ow*)
+let eps = 1e-8
 let compare_eps a b = a -. eps < b && b < a +. eps
 
+
 (** Zwraca iloczyn skalarny wektorow ab i cd *)
-let iloczyn_skalarny ((ax, ay): point) ((bx, by): point) ((cx, cy): point) ((dx, dy): point) = 
+let iloczyn_skalarny (ax, ay) (bx, by) (cx, cy) (dx, dy) = 
 	let (a, b, c, d) = (ax -. bx, ay -. by, cx -. dx, cy -. dy) in 
 		(a *. c) +. (b *. d)
 
+
 (** Zwraca iloczyn wektorowy wektorow ab i cd *)
-let iloczyn_wektorowy ((ax, ay): point) ((bx, by): point) ((cx, cy): point) ((dx, dy): point) = 
+let iloczyn_wektorowy (ax, ay) (bx, by) (cx, cy) (dx, dy) = 
 	let (a, b, c, d) = (ax -. bx, ay -. by, cx -. dx, cy -. dy) in 
 		(a *. d) -. (b *. c)
+
 
 (** [prostokat p1 p2] zwraca kartkę, reprezentującą domknięty prostokąt
 	o bokach równoległych do osi układu współrzędnych i lewym dolnym rogu [p1]
@@ -26,31 +30,38 @@ let iloczyn_wektorowy ((ax, ay): point) ((bx, by): point) ((cx, cy): point) ((dx
 	w pozostałych przypadkach 0 razy *)
 let prostokat ((ax, ay): point) ((cx, cy): point) : kartka = 
 	fun ((x, y): point) -> 
-		if ax -. eps <= x && x <= cx +. eps && ay -. eps <= y && y <= cy +. eps then 1 else 0
+		if ax -. eps <= x && x <= cx +. eps && ay -. eps <= y && y <= cy +. eps
+			then 1
+		else 0
+
 
 (** Zwraca odleglosc pomiedzy punktami a i b na plaszczyznie *)
 let odleglosc ((ax, ay): point) ((bx, by): point) = 
 	let (diff_x, diff_y) = (ax -. bx, ay -. by) in
 		sqrt (diff_x *. diff_x +. diff_y *. diff_y)
 
+
 (** [kolko o r] zwraca kartkę, reprezentującą kółko domknięte o środku w 
 		punkcie [o] i promieniu [r] *)
 let kolko (o: point) r : kartka = 
 	fun x -> if odleglosc o x <= r +. eps then 1 else 0
 
-(** [odbij_punkt one two v] zwraca punkt [v] odbity wzgledem prostej wyznaczonej 
-	przez punkty [one] i [two] *)
+
+(** [odbij_punkt one two v] zwraca punkt [v] odbity wzgledem prostej
+	wyznaczonej przez punkty [one] i [two] *)
 let odbij_punkt (one: point) (two: point) (v: point) = 
 	let (x, y) = (fst two -. fst one, snd two -. snd one) in
 	let mnoznik = 2. *. (iloczyn_skalarny v one two one) in
 	let mnoznik = mnoznik /. (iloczyn_skalarny two one two one) in
 	let wynik = (mnoznik *. x, mnoznik *. y) in
-		(fst wynik -. fst v +. (2. *. fst one), snd wynik -. snd v +. (2. *. snd one))
+		(fst wynik -. fst v +. (2. *. fst one),
+		 snd wynik -. snd v +. (2. *. snd one))
+
 
 (** [zloz p1 p2 k] składa kartkę [k] wzdłuż prostej przechodzącej przez
 	punkty [p1] i [p2] (muszą to być różne punkty). Papier jest składany
-	w ten sposób, że z prawej strony prostej (patrząc w kierunku od [p1] do [p2])
-	jest przekładany na lewą. Wynikiem funkcji jest złożona kartka. Jej
+	w ten sposób, że z prawej strony prostej (patrząc w kierunku od [p1] do
+	[p2]) jest przekładany na lewą. Wynikiem funkcji jest złożona kartka. Jej
 	przebicie po prawej stronie prostej powinno więc zwrócić 0.
 	Przebicie dokładnie na prostej powinno zwrócić tyle samo,
 	co przebicie kartki przed złożeniem. Po stronie lewej -
@@ -63,7 +74,9 @@ let zloz (one: point) (two: point) (a: kartka) : kartka =
 			else if  iloczyn_wektorowy two one v one < 0. then 0
 			else (a v) + (a (odbij_punkt one two v))
 
-(** [skladaj [(p1_1,p2_1);...;(p1_n,p2_n)] k = zloz p1_n p2_n (zloz ... (zloz p1_1 p2_1 k)...)]
+
+(** [skladaj [(p1_1,p2_1);...;(p1_n,p2_n)] k = zloz p1_n p2_n
+	(zloz ... (zloz p1_1 p2_1 k)...)]
 	czyli wynikiem jest złożenie kartki [k] kolejno wzdłuż wszystkich prostych
 	z listy *)
 let skladaj (lista: (point * point) list) kartka = 
